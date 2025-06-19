93df5a95d13527a82080a74f8ad66342b2e0aab9
Anxo Rodriguez <anxolin@gmail.com>
2025-06-19 17:07:17 +0100

    Add uniswap pool and scripts

--- script/UniswapV2Factory.s.sol
+++ script/UniswapV2Factory.s.sol
@@ -2,17 +2,17 @@
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
-import {Counter} from "../src/Counter.sol";
+import {UniswapV2Factory} from "../src/UniswapV2Factory.sol";
 
-contract CounterScript is Script {
-    Counter public counter;
+contract UniswapV2FactoryScript is Script {
+    UniswapV2Factory public uniswapV2Factory;
 
     function setUp() public {}
 
     function run() public {
         vm.startBroadcast();
 
-        counter = new Counter();
+        uniswapV2Factory = new UniswapV2Factory(address(0));
 
         vm.stopBroadcast();
     }
